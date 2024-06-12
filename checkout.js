9318066de6dfe4ff7afa444337b18edeb97bddbd
hiarica07 <halilarica@gmail.com>
2024-06-12 10:25:26 +0300

    Start

---
+++ checkout.js
@@ -0,0 +1,18 @@
+//!! Döngüler ile event listener tanımlanabilir. Az sayıda element var ise sorun olmaz ama çok fazla element var ise bu kaynak tüketimini arttırır.
+
+// const minusbtn = document.querySelectorAll(".fa-minus")
+// const plusbtn = document.querySelectorAll("fa-plus")
+
+// // console.log(minusbtn); // yakalayabildik mi ona bakıyoruz.
+
+// minusbtn.forEach((minus)=>{
+//     minus.addEventListener("click", ()=>{
+//         console.log("-");
+//     })
+// })
+
+// plusbtn.forEach((plus)=>{
+//     minus.addEventListener("click", ()=>{
+//         console.log("+");
+//     })
+// })
